d6cb5c26ecabd7104f43996c5288e4d0de9815c2
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-18 15:21:14 +0100

    second initial commit after problems with git, prepared environment to work

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Navigation = () => (
+
+    <nav>
+        <span>Ania Wojtkiewicz</span>
+        <ul>
+            <li>Home</li>
+            <li>Galeria</li>
+            <li>O mnie</li>
+            <li>Kontakt</li>
+            <li>Oferta</li>
+        </ul>
+    </nav>
+)
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const AboutPage = () => (
+    <Layout>
+        <h1>About Page</h1>
+    </Layout>
+);
+
+
+export default AboutPage;
+
