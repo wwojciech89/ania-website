95a729d54232e6a15301015f69f0940d1f59ae24
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-24 19:26:18 +0100

    add styles to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,42 @@ const SliderWrapper = styled.div`
 align-items: center;
 background-color: lime;
 height: 100vh;
+background-color: blue;
+max-width: 1400px;
+margin: auto;
+
+  div {
+    background-color: green;
+  }
 
   p {
     margin: 0;
   }
+
+`
+const PhotoTitle = styled.div`
+    background-color: white;
+    position: absolute;
+    bottom: 100rem;
+    left: 2rem;
+
+    p {
+      margin: 0;
+    }
 `
 
 
 
-
-const MainSlider = () => {
- return ( <SliderWrapper>
+const MainSlider = () => (
+ <SliderWrapper>
+    <div>
     <p>Plenery</p>
     <h1>Sesja Andrzeja i Justyny</h1>
     <p>Zobacz więcej</p>
-
+    </div>
   </SliderWrapper>
  )
-}
+
 
 
 
